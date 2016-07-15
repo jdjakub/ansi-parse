d783a87613cedfb4043b8c0e4a44c9b7f9fa9581
jdjakub <jdj27@cam.ac.uk>
2016-07-15 15:58:53 +0100

    Partitioned into Concrete (text) and Abstract (tree) modules

--- ansiparse.mli
+++ ansiparse.mli
@@ -1,20 +1,45 @@
-open Angstrom
-
 type color = Black | Red | Green | Yellow | Blue | Magenta | Cyan | White
 
-type style = Reset | Bold | Faint | Italic | Underline | Blink | Inverse | Hidden
-           | Strike | Fore of color | Back of color | Unknown of int
+module Concrete = struct
+  type style = Reset | Bold | Faint | Italic | Underline | Blink | Inverse | Hidden
+             | Strike | Fore of color | Back of color | Unknown of int
+  
+  (* Grammar:
+     Item --> Escape | Text
+     Escape --> csi Styles? cst
+     Styles --> Style ( ';' Style )*
+     Style --> dig+
+     Text --> char*
+  *)
+  
+  type t = Esc of style list | Text of string
+  
+  val item : t Angstrom.t
+end
 
-(* Grammar:
-   Item --> Escape | Text
-   Escape --> csi Styles? cst
-   Styles --> Style ( ';' Style )*
-   Style --> dig+
-   Text --> char*
-*)
+module Abstract = struct
+  type weight = Normal | Bold | Faint
+  type style = { weight     : weight
+               ; italic     : bool
+               ; underline  : bool
+               ; blink      : bool
+               ; reverse    : bool
+               ; strike     : bool
+               ; foreground : color option
+               ; background : color option
+               }
+  type 'a t = Base of 'a | Styled of style * 'a t list
 
-type item = Esc of style list | Text of string
+  val default = { weight     = Normal
+                ; italic     = false
+                ; underline  = false
+                ; blink      = false
+                ; reverse    = false
+                ; strike     = false
+                ; foreground = None
+                ; background = None
+                }
 
-val item : item t
+end
 
-type run 
+val parse : string -> string Abstract.t
