dd590d74800b1658249d940df2f41848f18a2c4f
jdjakub <jdj27@cam.ac.uk>
2016-08-30 12:25:48 +0100

    Removed _build directory

--- _build/driver/driver.ml
+++
@@ -1,9 +0,0 @@
-open Ansiparse
-
-let concrep = Concrete.parse stdin
-
-let absrep = Abstract.parse concrep
-
-let html = Html.of_tree absrep
-
-let () = Format.printf "%a@." (Tyxml.Html.pp_elt ()) html
--- _build/test/driver.ml
+++
@@ -1,5 +0,0 @@
-open Ansiparse
-
-let absrep = Abstract.parse stdin in
-let html = Html.of_tree absrep in
-let () = Tyxml.Html.pp_elt () (formatter_of_out_channel stdout) html
