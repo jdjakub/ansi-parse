a0db4e48548731d3b53720c89e1bee0211115c14
jdjakub <jdj27@cam.ac.uk>
2016-11-09 20:21:58 +0000

    Docs and packaging

--- ansiparse.mli
+++ ansiparse.mli
@@ -1,5 +1,10 @@
+(** Parse ANSI escape sequences into HTML *)
+
 type color = Black | Red | Green | Yellow | Blue | Magenta | Cyan | White
 
+(**
+The [Concrete] module is concerned with converting between raw text and a "concrete" list representation of the items in the text.
+*)
 module Concrete :
 sig
   type style = Bold | Faint | Italic | Underline | Blink | Inverse | Hidden
@@ -15,10 +20,17 @@ sig
 
   type t = Esc of style list | Reset | Text of string
 
+
+  (** Picture the text as ANSI escape sequences, interspersed with normal text. [parse] converts the raw characters to a [t list]. The 'reset' escape converts to [Reset]; all others convert to [Esc styles], and text to [Text str]. *)
   val parse : in_channel -> t list
+
+  (** Analogous to [parse]. *)
   val parse_str : string -> t list
 end
 
+(**
+This module translates concrete item lists into an abstract tree structure.
+*)
 module Abstract :
 sig
   type weight = Normal | Bold | Faint
@@ -31,14 +43,20 @@ sig
                ; foreground : color option
                ; background : color option
                }
+
   type 'a t = Base of 'a | Styled of style * 'a t list
+  
+  (** Styles are applied to ['a]s or further subtrees. *)
 
   val default : style
+  (** The style of the root of the structure; gives plain text *)
 
   val parse : Concrete.t list -> string t
+  (** Converts item lists into trees of styled text. Styles applied to strings will nest until a [Reset] is encountered, after which a new branch from the root is begun. *)
 
 end
 
+(** Backend for style trees -> HTML [<pre>] block *)
 module Html :
 sig
   (* functions from string Abstract.t -> Html elements *)
